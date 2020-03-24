fdb46c1b3a9ef0c6bb2de58939d3c162d3d133b7
Antti Ollikkala <antti@huom.io>
2020-03-24 19:05:49 +0200

    added tests to maksukortti

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,42 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoAlussaOikein() {
+        assertTrue(kortti.saldo() == 10);
+    }
+    
+    @Test
+    public void lataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(10);
+        assertTrue(kortti.saldo() == 20);
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinJosRahaaOnTarpeeksi() {
+        kortti.otaRahaa(5);
+        assertTrue(kortti.saldo() == 5);
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaaEiOleTarpeeksi() {
+        kortti.otaRahaa(15);
+        assertTrue(kortti.saldo() == 10);
+    }
+    
+    @Test
+    public void metodiPalauttaaTrueJosRahatRiittivat() {
+        assertTrue(kortti.otaRahaa(5));
+    }
+    
+    @Test
+    public void metodiPalauttaaFalseJosRahatEivatRiittaneet() {
+        assertTrue(!kortti.otaRahaa(15));
+    }
+    
+    @Test
+    public void korttiTulostaaRahamaaranOikein() {
+        assertEquals(kortti.toString(), "saldo: 0.10");
+    }
 }
